34435d54e30e49b5e025710e97969a47cd45f8d2
Yohey Sika Watanabe <yohey.watanave@gmail.com>
2022-10-06 15:39:17 +0900

    auto calculattion

--- src/components/SimulateSavings.vue
+++ src/components/SimulateSavings.vue
@@ -9,15 +9,7 @@
       <b-field label="一頭当たり平均重量（kg/年）" >
           <b-input type="number" v-model="average_weight"></b-input>
       </b-field> 
-      <section class="ingridient_ratio">
-        <b-field label="一頭当たりの廃棄量の割合（％）">
-            <b-slider v-model="garbage_ratio"></b-slider>
-        </b-field>
         
-        <b-field label="一頭当たりの投入可能内臓量の割合（％）">
-            <b-slider v-model="ingridient_ratio"></b-slider>
-        </b-field>      
-      </section>   
       
       <b-field label="廃棄単価（円/kg）">
         <b-input type="number" v-model="unitcost_garbage"></b-input>
@@ -37,16 +29,28 @@
       </b-field>
       <b-field label="都市ガス単位料金（円/㎥）" v-show="gas_type=='city'">
         <b-input type="number" v-model="unitcost_citygas"></b-input>
-      </b-field>      
+      </b-field>            
+      <b-field label="詳細入力モード">
+            <b-switch v-model="isDetail"></b-switch>
+      </b-field>
+      <section class="ingridient_ratio" v-show="isDetail">
+        <b-field label="現在の一頭当たりの廃棄量の割合（％）">
+            <b-slider v-model="garbage_ratio_percent"></b-slider>
+        </b-field>
+        
+        <b-field label="導入時の一頭当たりの投入可能内臓量の割合（％）">
+            <b-slider v-model="ingridient_ratio_percent"></b-slider>
+        </b-field>      
+      </section> 
+      <div class="generated" v-show="is_generate">
+        <p>節約できる金額は{{SimulatedSavingsEnergyCost}}円/年です。</p>
+        <p>概算になりますので、詳しいお見積りはご相談の際に共有させていただきます。</p>
+        <b-button @click="contact" type="is-primary is-light">お問い合わせはこちら</b-button>
+      </div> 
       <b-button @click="simulate" type="is-primary">計算する</b-button>
-      <b-message 
-            title="節約できる金額" 
-            type="is-success" 
-            aria-close-label="結果を消す"
-            v-show="is_generate">
-            ”シカくん初号機”を導入することで節約できる光熱費は 約{{SimulatedSavingsEnergyCost}}円です。※概算になるため、詳しいお見積りはヒアリング後にさせて頂きます。
-            <b-button @click="contact" type="is-success">お問い合わせはこちら</b-button>
-        </b-message>
+      <b-button @click="reset" type="is-primary is-light">リセットする</b-button>            
+           
+      
   </div>
 </template>
 
@@ -64,49 +68,86 @@ export default {
   },
   data() {
             return {
-                value: 5,
-                deer_numbers:150,
-                average_weight:60,
-                garbage_ratio:0.7,
-                ingridient_ratio:0.5,
-                unitcost_garbage:35,
-                gas_type: "propan",
-                unitcost_citygas: 250,
-                unitcost_propangas: 690,
-                is_generate:false,
-                SimulatedSavings:false,
-                amount_gas:false,
-                gabage_amount:false,
-                ingridient_amount:false,
-                allcost_garbage:false,
-                SimulatedSavingGarbageCost:false,
-                SimulatedSavingsPropangasCost:false,
-                SimulatedSavingsCitygasCost:false
-
+              // input data
+              deer_numbers:150,
+              average_weight:60,
+              garbage_ratio_percent:70,              
+              ingridient_ratio_percent:50,              
+              unitcost_garbage:35,
+              unitcost_propangas: 690,
+              unitcost_citygas: 250,
+              // var
+              dry_weight: 0.3,
+              vs_ratio: 0.8,
+              vs_weight: false,
+              ratio_metan: 0.527,
+              // function
+              is_generate:false,
+              gas_type: "propan",
+              // SimulatedSavingsEnergyCost: "???",
+              isDetail: false,
             }
-        },   
-  methods : {
-      simulate(){      
-        this.is_generate = true;
-        this.gabage_amount = this.deer_numbers * this.average_weight * this.garbage_ratio;
-        this.ingridient_amount = this.deer_numbers * this.average_weight * this.ingridient_ratio;
-        this.allcost_garbage = this.gabage_amount * this.unitcost_garbage;
-        this.amount_gas = this.ingridient_amount * 0.3 * 1000 * 0.8 * 0.527 / 1000 //(m3) 
-        this.SimulatedSavingGarbageCost = this.allcost_garbage * this.ingridient_ratio;
-        this.SimulatedSavingsPropangasCost = this.amount_gas * this.unitcost_propangas;
-        this.SimulatedSavingsCitygasCost = this.amount_gas * this.unitcost_citygas;
-        
-        
-        if(this.gas_type=="city"){
-          this.SimulatedSavingsEnergyCost = this.SimulatedSavingsCitygasCost.toLocaleString();          
-          
-        }else if(this.gas_type=="propan"){
-          this.SimulatedSavingsEnergyCost = this.SimulatedSavingsPropangasCost.toLocaleString();
-          
+        },
+  computed:{
+    garbage_ratio:function(){
+        return this.garbage_ratio_percent/100
+    },
+    ingridient_ratio:function(){
+        return this.ingridient_ratio_percent/100
+    },
+    gabage_amount: function(){
+      return this.deer_numbers * this.average_weight * this.garbage_ratio;
+    },
+    amount_ingridient: function(){
+      return this.deer_numbers * this.average_weight * this.ingridient_ratio;
+    },
+    allcost_garbage: function(){
+      return this.gabage_amount * this.unitcost_garbage;
+    },
+    amount_gas: function(){
+      return this.amount_ingridient * this.dry_weight *  this.vs_ratio * this.ratio_metan //(m3) 
+    },
+    // output
+    SimulatedSavingGarbageCost: function() {
+      return Math.round(this.allcost_garbage * this.ingridient_ratio);
+    },
+    SimulatedSavingsPropangasCost: function(){
+      return Math.round(this.amount_gas * this.unitcost_propangas);
+    },
+    SimulatedSavingsCitygasCost: function(){
+      return Math.round(this.amount_gas * this.unitcost_citygas);        
+    },
+    SimulatedSavingsEnergyCost: function(){
+
+      if(this.gas_type=="city"){          
+          return this.SimulatedSavingsCitygasCost.toLocaleString();                    
+        }else if(this.gas_type=="propan"){          
+          return this.SimulatedSavingsPropangasCost.toLocaleString();          
         }else{
-          this.SimulatedSavingsEnergyCost = "大変申し訳ございません。エラーが発生しました。"
+          return "大変申し訳ございません。エラーが発生しました。"
         }
+    }
+  },
+  methods : {
+      simulate:function(){      
+        this.is_generate = true;  
+
+        // if(this.gas_type=="city"){
+          
+        //   this.SimulatedSavingsEnergyCost = this.SimulatedSavingsCitygasCost.toLocaleString();                    
+        // }else if(this.gas_type=="propan"){
+          
+        //   this.SimulatedSavingsEnergyCost = this.SimulatedSavingsPropangasCost.toLocaleString();          
+        // }else{
+        //   this.SimulatedSavingsEnergyCost = "大変申し訳ございません。エラーが発生しました。"
+        // }
         
+      },
+      reset: function(){
+        this.SimulatedSavingsEnergyCost= "???"
+      },
+      contact: function(){
+        this.is_generate = !this.is_generate
       }
   }
 }
@@ -120,7 +161,8 @@ export default {
   
 }
 .ingridient_ratio{
-  display: none
+  /* display: none */
+  margin:1.5rem auto;
 }
 h3 {
   margin: 2rem 0 3rem;
@@ -144,9 +186,16 @@ a {
 .gas_change{
   margin: 1rem auto;
 }
+.generated{
+  margin: 4rem auto;
+  font-family: bold;
+}
+.simulate .field{
+  width: 100%
+}
 @media (min-width: 824px) {
   .simulate{
-    width: 70%;
+    width: 30%;
     display: flex;
     justify-content: center;
     flex-direction: column;
